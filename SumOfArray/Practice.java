073ba11186ebebb50be500dba0cb854e66721e19
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-02-28 17:53:27 +0530

    This is to find sum of array

---
+++ SumOfArray/Practice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Practice{
+  public static void main(String[] args){
+    int sum = 0;
+    int[] arr = {1,2,3,4,5,6,7,8,9,10};
+    for(int i=0;i<arr.length;i++){
+      sum = sum + arr[i];
+    }
+    System.out.print("The sum of array is :- "+sum);
+  }
+}
+
+//  Feeteen Question 
