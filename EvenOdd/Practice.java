c981d3fc29cfadd430af829019f8ef4b260040d7
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-03-03 18:12:27 +0530

    This is for checking whether the given number is  even odd odd

---
+++ EvenOdd/Practice.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Practice{
+  public static void main(String[] args){
+    int num = 10;
+    if(num % 2 == 0){
+      System.out.println(num+" is even number");
+    }else{
+      System.out.println(num+" is odd number");
+    }
+  }
+}
