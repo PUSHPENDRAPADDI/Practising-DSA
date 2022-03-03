b41a77f29cada0c424a3dcde805600816d5f4cbc
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-03-03 18:15:15 +0530

    This is to check whether the given number is positive or negative

---
+++ PositiveNegative/Practice.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Practice{
+  public static void main(String[] args){
+    int num = -10;
+    if(num > 0){
+      System.out.println(num+" is positive number");
+    }else{
+      System.out.println(num+" is negative number");
+    }
+  }
+}
