69ed5842d1813d0b95feffd7c10b5458955ae0fe
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-03-04 21:18:51 +0530

    This is for finding sum of Nth natural number

---
+++ SumNatural/Practice.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Practice{
+   public static void main(String[] args){
+    int num = 5, sum = 0;
+    for(int i = 1;i<= num;i++){
+        sum = sum+ i;
+    }
+      System.out.println(sum);
+  }
+}
