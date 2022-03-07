4559357ece809bd80aa067b72fc52eb732273407
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-03-07 19:18:46 +0530

    This is to find the sum of numbers in given range

---
+++ SumOfNumberInRange/Practice.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Practice{
+  public static void main(String[] args){
+    int num1 = 2, num2 = 7, sum = 0;
+    for(int i = num1; i<=num2;i++){
+      sum += i;
+    }
+    System.out.println(sum);
+  }
+}
