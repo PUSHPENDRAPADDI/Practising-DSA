6d4fc8ac085631cd110b3e2d9fb3382f9b76a632
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-03-08 18:58:43 +0530

    This is to reverse the given string

---
+++ ReverseString/Practice.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Practice{
+  public static void main(String[] args){
+    String str = "HELLO ji";
+    for(int i = str.length()-1;i>=0;i--){
+      System.out.print(str.charAt(i));
+    }
+  }
+}
