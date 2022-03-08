342506bd9a74285dec50347989a5de1bf198a894
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-03-08 18:43:30 +0530

    This is to find the ASCII value of characters

---
+++ ASCIIValue/Practice.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Practice{
+  public static void main(String[] args){
+    char ch = 'A';
+    System.out.println((int)ch);
+  }
+}
