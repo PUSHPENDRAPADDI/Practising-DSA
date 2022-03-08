0cc12cd1bda17b647b88bec1d565441abfd18bb9
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-03-08 18:46:25 +0530

    This is to remove all the vowels present in the given strings

---
+++ RemoveVowel/Practice.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Practice{
+  public static void main(String[] args){
+     String str = "take u forword";
+     str = str.toLowerCase();
+     for(int i =0; i< str.length();i++){
+       if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' ||   str.charAt(i) == 'u'){
+        str = str.substring(0,i) + str.substring(i+1);
+        i--;
+       }
+     }
+    System.out.println(str);
+   }
+}
