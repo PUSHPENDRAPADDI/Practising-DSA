6f3769ca06e2975d8bb5323642676ac7c31a7e57
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-03-13 21:18:19 +0530

    This is to  remove the duplicates from the given string

---
+++ RemoveDuplicates/Practice.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Practice{
+  public static void main(String[] args){
+    String str = "bbccaabbz";
+    int[] ch = new int[26];
+    for(int i = 0;i<str.length();i++){
+      ch[str.charAt(i) - 'a']++;
+    }
+    for(int i = 0;i<26;i++){
+      if(ch[i] > 0){
+        System.out.println((char)(i + 'a'));
+      }
+    }
+  }
+}
