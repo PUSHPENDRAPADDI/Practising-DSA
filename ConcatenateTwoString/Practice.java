bea461fef743f97db74a53ca764edf933ae25aea
PUSHPENDRA YADAV <Sunnyrony3011@gmail.com>
2022-03-14 20:38:06 +0530

    This is to concatenate two string

---
+++ ConcatenateTwoString/Practice.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Practice{
+ public static void main(String[] args){
+    String str = "Hello";
+    String str2 = "World";
+    String str3 = str + str2;
+    System.out.print(str3);
+  }
+}
